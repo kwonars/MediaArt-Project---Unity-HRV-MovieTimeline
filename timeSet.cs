5ad9aea01478877d55cf55bdeb3b4e100e57a1cd
kwonars <kwonars@gmail.com>
2023-04-12 19:41:51 +0900

    Add files via upload

---
+++ timeSet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// For child HrvActor object
+public class timeSet : MonoBehaviour
+{
+    public string startTime;
+    public string movieLength;
+    public List<TimeSpan> timeData = new List<TimeSpan>();
+    public TimeSpan startTimeTimeSpan;
+    public TimeSpan movieLengthTimeSpan;
+
+    // Start is called before the first frame update
+    public List<TimeSpan> getTimeData()
+    {        
+        float.TryParse(startTime, out float startTimeFloat);
+        long startTimeLong = Convert.ToInt64(startTimeFloat*10000000f);
+        startTimeTimeSpan = new TimeSpan(startTimeLong);
+        timeData.Add(startTimeTimeSpan);
+
+        float.TryParse(movieLength, out float movieLengthFloat);
+        long movieLengthLong = Convert.ToInt64(movieLengthFloat*10000000f);
+        movieLengthTimeSpan = new TimeSpan(movieLengthLong);
+        timeData.Add(movieLengthTimeSpan);
+        
+        // Debug.Log(timeData);
+        return timeData;
+    }
+}
---
+++ timelineAdjustment.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// For parent Actors object
+public class timelineAdjustment : MonoBehaviour
+{
+    [HideInInspector]
+    DateTime initialTime;
+    TimeSpan elapsedTime;
+    List<TimeSpan> allStartTime = new List<TimeSpan>();
+    List<TimeSpan> allMovieLength = new List<TimeSpan>();
+    List<List<TimeSpan>> allTimeData = new List<List<TimeSpan>>();
+    List<bool> initialPlay = new List<bool>();
+    int numberOfChildren;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialTime = DateTime.Now;
+        numberOfChildren = transform.childCount;
+        // Debug.Log(numberOfChildren);
+
+        for (int i = 0; i < numberOfChildren; i++)
+        {
+            allTimeData.Add(transform.GetChild(i).gameObject.GetComponent<timeSet>().getTimeData());
+            allStartTime.Add(allTimeData[i][0]);
+            allMovieLength.Add(allTimeData[i][1]);
+            
+            transform.GetChild(i).gameObject.SetActive(false);
+            initialPlay.Add(true);
+            // Debug.Log(i);
+            // Debug.Log(allStartTime[i]);
+            // Debug.Log(allMovieLength[i]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsedTime = DateTime.Now - initialTime;
+        for (int i = 0; i < numberOfChildren; ++i)
+        {
+            if (elapsedTime > allStartTime[i] && elapsedTime <= allStartTime[i] + allMovieLength[i])
+            {
+                transform.GetChild(i).gameObject.SetActive(true);
+                if(initialPlay[i])
+                {
+                    transform.GetChild(i).gameObject.GetComponent<HVR.HvrActor>().timestampProvider.Play();
+                    initialPlay[i] = false;
+                }
+            }
+            else if (elapsedTime > allStartTime[i] + allMovieLength[i])
+            {
+                transform.GetChild(i).gameObject.SetActive(false);
+            }
+        }
+    }
+}
